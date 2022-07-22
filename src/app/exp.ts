ca7ecf90f0ba2b4c4498bb678ca043c74fa0666f
Fede <efesjotaseles2@gmail.com>
2022-07-22 18:22:30 -0300

    Testing experiencias in component

---
+++ src/app/exp.ts
@@ -0,0 +1,19 @@
+export interface Exp {
+    titulo : string;
+    detalle : string;
+}
+
+export const experiencias = [
+    {
+        titulo : 'Administrador',
+        detalle : 'En Administración Victor Eloy Echeverría, desde 2011 hasta 2021.'
+    },
+    {
+        titulo : 'Cámara y edición',
+        detalle : 'En Canfra Producciones, desde 2010 hasta 2011.'
+    },
+    {
+        titulo : 'Atención al público',
+        detalle : 'En Librería José Lanci.'
+    }
+];
