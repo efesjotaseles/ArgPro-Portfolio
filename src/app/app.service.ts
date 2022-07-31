aef5034cb429b58133a6c7175db1949912da4186
Fede <efesjotaseles2@gmail.com>
2022-07-31 19:10:30 -0300

    Attempt to comm with backend on header

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+export interface AcercaDe {
+  message: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
-export class AppService {
 
-  constructor() { }
+export class AppService {
+  rootURL = 'localhost:8080';
+
+
+  constructor(private http: HttpClient) { }
+
+  getAcercaDe(){
+    return this.http.get<AcercaDe>(this.rootURL+'/acercade');
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { AcercaDe, AppService } from '../app.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  providers: [AppService]
 })
 export class HeaderComponent implements OnInit {
 
-  acercaDe : string = "'PLACEHOLDER'";
+  acercaDe : AcercaDe | undefined;
 
-  constructor() { }
+  showAcercaDe(){
+    this.appService.getAcercaDe().subscribe((data: AcercaDe) => this.acercaDe ={...data});
+  }
+
+  constructor(private appService: AppService) { }
 
   ngOnInit(): void {
     console.log("Header works!");
