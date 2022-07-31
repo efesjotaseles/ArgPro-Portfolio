293d09cfbb8eb569870fc9b93dd1b5de76031316
Fede <efesjotaseles2@gmail.com>
2022-07-31 18:26:28 -0300

    Added app.service for comm with back

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -20,7 +19,6 @@ import { CvInfocardComponent } from './cv-infocard/cv-infocard.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
